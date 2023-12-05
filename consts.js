db7689622797979adc8d2f79e13d8cb01347de64
AGSTRANGER <hannibaletunisien@gmail.com>
2023-12-05 14:04:08 +0100

    first commit

---
+++ consts.js
@@ -0,0 +1,30 @@
+const units = [
+  "zéro",
+  "un",
+  "deux",
+  "trois",
+  "quatre",
+  "cinq",
+  "six",
+  "sept",
+  "huit",
+  "neuf",
+];
+const teens = ["dix", "onze", "douze", "treize", "quatorze", "quinze", "seize"];
+const tens = [
+  "",
+  "",
+  "vingt",
+  "trente",
+  "quarante",
+  "cinquante",
+  "soixante",
+  "soixante-dix",
+  "quatre-vingts",
+  "quatre-vingt-dix",
+];
+
+exports.units = units;
+exports.teens = teens;
+exports.tens = tens;
+exports.hundreds = hundreds;
---
+++ main.js
@@ -0,0 +1,3 @@
+const { inputNumbers } = require("./dataSet");
+const { convertListToFrench } = require("./services");
+const french_numbers = convertListToFrench(inputNumbers);
